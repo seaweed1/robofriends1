f94a51b18a6dca48fc4d4d94ad4d753549248c45
System Administrator <root@williams-imac-2.home>
2020-03-28 12:46:34 -0400

    Updates

---
+++ precache-manifest.eb2b555da986a8e7e5e3fbbd6865abe7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a86674d41e2021c6d38b4937a7166ff",
+    "url": "/robofriends1/index.html"
+  },
+  {
+    "revision": "ad6730d9a443bf7796aa",
+    "url": "/robofriends1/static/css/main.4940ac32.chunk.css"
+  },
+  {
+    "revision": "81f42eb8c98e5774d25c",
+    "url": "/robofriends1/static/js/2.76668530.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends1/static/js/2.76668530.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad6730d9a443bf7796aa",
+    "url": "/robofriends1/static/js/main.c4265229.chunk.js"
+  },
+  {
+    "revision": "37bdbc4ffa5b6ae0a094",
+    "url": "/robofriends1/static/js/runtime-main.8903bc44.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends1/static/media/SEGA.12a3db8b.woff"
+  }
+]);
